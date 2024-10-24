76d49b6d3084f8385e13f1cf54df9dede74b2682
Forcica <47173413+Forcica@users.noreply.github.com>
2024-10-24 17:05:59 +0200

    [ADD] Added an animation of the camera when loading the page

--- src/components/Canvas3d.jsx
+++ src/components/Canvas3d.jsx
@@ -1,6 +1,7 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useEffect } from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls, useGLTF, PerspectiveCamera } from '@react-three/drei';
+import gsap from 'gsap';
 
 const Model = () => {
    const { scene } = useGLTF('/assets/models/scene.gltf');
@@ -20,22 +21,43 @@ const Model = () => {
       <primitive 
          object={scene} 
          scale={[0.04, 0.04, 0.04]} 
-         position={[-1, -1.5, 3]}
+         position={[-0.8, -1.1, 4]}
          rotation={[0, 4.9, 0]} 
       />
    );
- };
+};
 
+function CameraAnimation() {
+   const { camera } = useThree();
+   
+   useEffect(() => {
+   camera.position.set(0, 0, 20);
+   gsap.to(camera.position, {
+      duration: 2,
+      z: 10,
+      ease: "power2.inOut"
+   });
+   }, [camera]);
+
+   return null;
+}
+ 
 function Canvas3D() {
    return (
       <Canvas>
-         <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+         <CameraAnimation />
+         <PerspectiveCamera makeDefault position={[0, 0, 20]} />
          <ambientLight intensity={1} />
-         <directionalLight position={[0, 10, 5]} intensity={1.5} />
+         <directionalLight position={[0, 10, 5]} intensity={2.5} />
          <Suspense fallback={null}>
-         <Model />
+            <Model />
          </Suspense>
-         <OrbitControls />
+         <OrbitControls 
+            enableZoom={false}
+            enablePan={false}
+            enableRotate={true}
+            enabled={false}
+         />
       </Canvas>
    );
 }
